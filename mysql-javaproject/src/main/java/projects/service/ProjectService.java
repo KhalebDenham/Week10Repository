a04c93c8487cab8a7fce7ca6c252f7324a84acae
Khaleb Denham <60304297+KhalebDenham@users.noreply.github.com>
2024-04-11 17:04:58 -0400

    Add files via upload
    NOTE: ONLY ProjectsApp.java, ProjectService.Java, AND ProjectDao.Java WERE ADJUSTED/CHANGED IN THIS ASSIGNMENT.

---
+++ mysql-javaproject/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,26 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+
+		return projectDao.insertProject(project);
+	}
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+
+	}
+
+	public Project fetchProjectById(Integer projectID) {
+		return projectDao.fetchProjectById(projectID).orElseThrow(
+				() -> new NoSuchElementException("Project with project ID=" + projectID + " does not exist."));
+	}
+
+}
